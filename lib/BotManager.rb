a34ed7298c0519b5e2529b2601b6b79f5a505bc4
Stephen Robinson <stephen@stephen-robinson.co.uk>
2018-11-28 14:31:11 +0000

    require botmanager client and template config

--- lib/BotManager.rb
+++ lib/BotManager.rb
@@ -2,6 +2,8 @@ require "BotManager/version"
 require "BotManager/alexa"
 require "BotManager/lex"
 require "BotManager/parsers"
+require "BotManager/client"
+require "BotManager/template_config"
 
 module BotManager
   class Error < StandardError; end
